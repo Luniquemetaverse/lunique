78ac9a0fc935371a6e7c854f986dd964a89107e0
shahbaz48 <shahbaz@capabl.com>
2022-11-07 17:56:04 +0000

    code commit after counter update and added fucntion to push data to the google stylesheet when register

---
+++ src/components/pages-partials/home/index.js
@@ -0,0 +1,12 @@
+import SharedLayout from '@/components/layout/shared-layout'
+import HomeHero from '../../home-hero'
+
+const Home = () => {
+  return (
+    <SharedLayout title="Home">
+      <HomeHero />
+    </SharedLayout>
+  )
+}
+
+export default Home

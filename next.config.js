cbea0c9efaebb87f129ab2dfe37af5ec4f70c995
Kadir Atalı <m.sametatali@gmail.com>
2022-11-12 21:14:59 +0300

    fix build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
-module.exports = {
+/**
+ * @type {import('next').NextConfig}
+ */
+
+const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['lh3.googleusercontent.com', 'firebasestorage.googleapis.com'],
   },
 }
+
+module.exports = nextConfig

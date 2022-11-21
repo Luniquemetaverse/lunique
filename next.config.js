cd154223e36cf7c74b77faa11de905f31f8da5b0
samikhan97 <osaamasajid@gmail.com>
2022-11-21 18:57:18 +0500

    Update: Added Headers in next.config.js

--- next.config.js
+++ next.config.js
@@ -7,6 +7,24 @@ const nextConfig = {
   images: {
     domains: ['lh3.googleusercontent.com', 'firebasestorage.googleapis.com'],
   },
+
+  async headers() {
+    return [
+      {
+        source: '/demo',
+        headers: [
+          {
+            key: 'Accept-Encoding',
+            value: 'gzip',
+          },
+          {
+            key: 'Accept-Encoding',
+            value: 'br',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
